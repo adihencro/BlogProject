f892760908fe00fbaf5c5f07764c98e65f0e75e5
adihencro <adihencro1412@gmail.com>
2024-03-06 13:44:42 +0000

    create app inside the project

--- BlogDjangoProject/BlogDjangoProject/urls.py
+++ BlogDjangoProject/BlogDjangoProject/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+#from django.urls import re_path as url
+
 
 urlpatterns = [
+    path("", include("BlogApplication.urls")),
     path("admin/", admin.site.urls),
 ]
--- BlogDjangoProject/posts/apps.py
+++ BlogDjangoProject/posts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogapplicationConfig(AppConfig):
+class PostsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "BlogApplication"
+    name = "posts"
