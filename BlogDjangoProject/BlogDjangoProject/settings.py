c5f4c7ceb12dd1a8262c73b4fe926a400420349b
adihencro <adihencro1412@gmail.com>
2024-03-20 16:01:28 +0200

    pagination

--- BlogDjangoProject/BlogDjangoProject/settings.py
+++ BlogDjangoProject/BlogDjangoProject/settings.py
@@ -64,7 +64,10 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',  
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
+
 
 SWAGGER_SETTINGS = {
    'SECURITY_DEFINITIONS': {
